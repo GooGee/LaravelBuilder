6fa8b37194123bc64097f4f09dd323fefd37a306
GooGee <mou-gj@live.com>
2022-10-31 08:46:33 +0800

    0.1.3

--- src/main/kotlin/com/github/googee/laravelbuilder/Constant.kt
+++ src/main/kotlin/com/github/googee/laravelbuilder/Constant.kt
@@ -3,7 +3,7 @@ package com.github.googee.laravelbuilder
 class Constant {
     companion object {
 
-        const val DocumentationURI = "https://googee.github.io/Laravel-Builder-Page"
+        const val DocumentationURI = "https://googee.github.io/laravel-builder"
         const val InstallGuideURI = "$DocumentationURI#install"
         const val Title = "Laravel Builder"
     }
